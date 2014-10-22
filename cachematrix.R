dbc0900f11bce2285f36265cefaab6e602282061
elizasb <eliza.bradley@gmail.com>
2014-10-21 23:24:39 -0400

    Final - fixed spacing

--- cachematrix.R
+++ cachematrix.R
@@ -50,8 +50,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## by makeCacheMatrix above. If the inverse has already been calculated
 ## (and the matrix has not changed), then the cachesolve should retrieve
 ## the inverse from the cache.
-
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinverse()
